19ce6c006c5f78d8d2900d77eb98a64a145c57c2
18253388744 <2024027881@qq.com>
2020-12-23 11:11:08 +0800

    增加签到功能

--- src/main/java/com/wdsjol/servlet/home/QDServlet.java
+++ src/main/java/com/wdsjol/servlet/home/QDServlet.java
@@ -1,8 +1,8 @@
 package com.wdsjol.servlet.home;
 
 import com.wdsjol.config.SpringConfig;
+import com.wdsjol.entity.User;
 import com.wdsjol.service.impl.QDServiceImpl;
-import com.wdsjol.service.impl.ZyCzServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -11,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -24,12 +25,14 @@ public class QDServlet extends HttpServlet {
         request.setCharacterEncoding("utf-8");
         response.setCharacterEncoding("utf-8");
         PrintWriter out = response.getWriter();
+        HttpSession session = request.getSession();
+        User user =(User)session.getAttribute("user");
         ApplicationContext app = new AnnotationConfigApplicationContext(SpringConfig.class);
         QDServiceImpl qd = app.getBean(QDServiceImpl.class);
         if ("info".equals( request.getParameter("do"))){
-            out.print(qd.getInfo("18253388744"));
+            out.print(qd.getInfo(user.getUsername()));
         }else if ("qd".equals( request.getParameter("do"))){
-            out.print(qd.qd("18253388744"));
+            out.print(qd.qd(user.getUsername()));
         }
     }
 }
